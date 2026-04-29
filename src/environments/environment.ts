afbad5a45b36f7df6a2679f3b13ae78bcdbbc890
kingshukmath2014 <kingshukmath2014@gmail.com>
2026-04-29 16:29:57 +0530

    env fixed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,5 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:3000/',
-  imageUrl: 'http://localhost:3000/public/images/',
+  imageUrl: 'https://yogavidyaschool.com:3000/public/images/',
 };
