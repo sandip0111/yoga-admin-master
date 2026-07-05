e84cb3e740925a6582c8d1648d3f4b025075e4d5
kingshukmath2014 <kingshukmath2014@gmail.com>
2026-07-05 17:04:14 +0530

    feat: update image URL to use CloudFront for improved performance

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://api.yogavidyaschool.com/',
-  imageUrl: "https://api.yogavidyaschool.com/public/images/"
+  imageUrl: 'https://d29rwrqvux6m5p.cloudfront.net/images/'
 };
